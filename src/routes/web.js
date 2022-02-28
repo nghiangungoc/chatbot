6e51ba5545968da7049416fb5c05ebf7f61ef9d1
nghia <nghiangungoc>
2022-02-28 08:33:07 +0700

    new

---
+++ src/routes/web.js
@@ -0,0 +1,12 @@
+import express from "express";
+import homeController from "../controllers/homeControllers";
+
+let router = express.Router();
+let initWebRoutes = (app) => {
+    router.get("/", homeController.getHomePage);
+    app.post('/webhook', homeController.postWebhook);
+    app.get('/webhook', homeController.getWebhook);
+
+    return app.use('/', router);
+}
+module.exports = initWebRoutes;
