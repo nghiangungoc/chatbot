69cdcfef5119faeab8fc15b73a4b70d91810f274
nghia <nghiangungoc>
2022-03-11 11:33:32 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import request from "request";
 require('dotenv').config();
 
@@ -26,10 +27,32 @@ let callSendAPI = (sender_psid, response) => {
     });
 
 }
+let getUserName = async (sender_psid) => {
+    let username = '';
+    // Send the HTTP request to the Messenger Platform
+    await request({
+        "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "GET",
+    }, (err, res, body) => {
+        console.log(body);
+        if (!err) {
+            response = JSON.parse(res);
+            // "first_name":"peter",
+            // "last_name":"Chang",
+            username = `${response.first_name} ${response.last_name}`
+
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+    return username;
+}
 let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
-            response = { "text": "Ok. xin chào mừng bạn ABC đến với nhà hàng của nghĩa" }
+            let username = await getUserName(sender_psid);
+            response = { "text": `Ok. xin chào mừng bạn ${username} đến với nhà hàng của nghĩa` }
             await callSendAPI(response);
             resolve('done');
         } catch (e) {
